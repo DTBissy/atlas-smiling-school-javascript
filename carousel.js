4564c2380d84a3c7eedfbc10d2d6a96eba26ccf3
Dtbissy <isaactheedwards@gmail.com>
2024-12-01 16:02:36 -0600

    Gotta take a family break

--- carousel.js
+++ carousel.js
@@ -1,22 +1,29 @@
 $(function fetchSmileQuoteApi () {
+  $('.loader').show();
 
-var $quotes = $('.smiling-man')
-var $bQuote = $('#bQuote')
+  slick_single_quote();
 
+  var $loader = $('.loader')
+
+  $('.smiling-man').hide();
     $.ajax({
       url: 'https://smileschool-api.hbtn.info/quotes',
       type: 'GET',
       dataType: 'json',
+      timeout: 10000,
       success: function (data) {
-        console.log(data);
+        $('.loader').hide();
+        $('.smiling-man').show();
+
+
         $.each(data, function (i, item) {
           if (i === 0) {
-          // $('#pic').attr(`src=${pic_url}`)
+          $('#pic').attr('src', item.pic_url)
           $('#bQuote').text(`${item.text}`)
           $('#pName').text(`${item.name}`)
           $('#title').text(`${item.title}`)
         } else if (i === 1) {
-           // $('#pic').attr(`src=${pic_url}`)
+          $('#pic2').attr('src', item.pic_url)
            $('#bQuote2').text(`${item.text}`)
            $('#pName2').text(`${item.name}`)
            $('#title2').text(`${item.title}`)
@@ -28,3 +35,33 @@ var $bQuote = $('#bQuote')
       }
     });
   });
+
+function slick_single_quote () {
+    $('.smile-wrap_slide').slick({
+      arrows: true,
+      infinite: true,
+    });
+  }
+
+function multiple_qutoe() {
+  $
+}
+
+$.ajax({
+  url: "https://smileschool-api.hbtn.info/popular-tutorials",
+  type: 'GET',
+  data: 'json',
+  success: function(data){
+    $.each(data, function(i, item){
+      if (i === 0 ) {
+        $('#thumb_url_0').attr('src', item.thumb_url);
+        $('#title_0').text(`${item.title}`);
+        $('#sub-title_0').text(`${item.sub-title}`);
+        $('#author-pic_0').attr('src', item.author_pic_url);
+        $('#author_0').text(`${item.author}`)
+        $('#duration_0').text(`${item.duration}`)
+
+      }
+    })
+  }
+})
